ba1924b973821fef48cfdf1ee9f15bf5bc640db0
David Freifeld <freifeld.david@gmail.com>
2021-05-15 02:34:05 -0700

    Experiments with GUI layout

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -1,7 +1,9 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QGridLayout>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QGroupBox>
 #include <QtWidgets/QPushButton>
 #include <iostream>
 #include <../MyLib/measure.hpp>
@@ -11,51 +13,54 @@
  
 int main(int argc, char *argv[]) 
 {
-    double cpu_baseline = collect_baseline_mW(cpu_uJ, std::chrono::milliseconds(5000));
-	double gpu_baseline = collect_baseline_mW(gpu_uJ, std::chrono::milliseconds(5000));
-	auto prev_cpu = cpu_uJ();
-	auto prev_gpu = gpu_uJ();      
-    auto start = std::chrono::high_resolution_clock::now();
-	// char* args[2] = {NULL};
     // Profiler prof(strtoll(argv[1], NULL, 10));
     // prof.start();   
 	// prof.dump(prof.funcs, 0);
-    QApplication app(argc, argv);
- 
+    QApplication app(argc, argv);   
     QWidget widget;
     widget.resize(640, 480);
     widget.setWindowTitle("Hello, world!!!");
-    
+
     QGridLayout *gridLayout = new QGridLayout(&widget);
 
-    // 	// std::ostringstream os;
-    // 	// os << baseline;
-    QLabel* label = new QLabel(QString::fromStdString("Baseline mW: "));
+    QLabel* label = new QLabel(QString::fromStdString("envopt"));
 
-    QPushButton* btn1 = new QPushButton("Push Me");
-    QObject::connect(btn1, &QPushButton::released, &widget,
-        []() 
-        { 
-            app::Window1* dialog = new app::Window1();
-            dialog->setModal(true);
-            dialog->setWindowModality(Qt::ApplicationModal);
-            dialog->exec();
-            delete dialog;
-        });
+    QPushButton* close = new QPushButton("Close");
+	QPushButton* open = new QPushButton("Open Log");
+	QPushButton* retry = new QPushButton("Re-record");	
 
-    label->setAlignment(Qt::AlignVCenter | Qt::AlignHCenter);
-    gridLayout->addWidget(label);
-    gridLayout->addWidget(btn1);
+	QObject::connect(close, &QPushButton::released, &QApplication::quit);	
+	
+    // QObject::connect(btn1, &QPushButton::released, &widget,
+    //     []() 
+    //     { 
+    //         app::Window1* dialog = new app::Window1();
+    //         dialog->setModal(true);
+    //         dialog->setWindowModality(Qt::ApplicationModal);
+    //         dialog->exec();
+    //         delete dialog;
+    //     });
+
+    label->setAlignment(Qt::AlignVCenter);
+
+	QGroupBox* groupbox = new QGroupBox("envopt: indev 0.0");
+	groupbox->setFlat(true);
+	QVBoxLayout* vbox = new QVBoxLayout;
+	QGroupBox* targets = new QGroupBox("Primary Offenders");
+	QGroupBox* opt = new QGroupBox("Optimization Strategies");
+	QGroupBox* env = new QGroupBox("Environmental Impact");
+	vbox->addWidget(targets);
+	vbox->addWidget(opt);
+	vbox->addWidget(env);
+	groupbox->setLayout(vbox);
+    gridLayout->addWidget(groupbox, 0, 0, 1, 3);
+    gridLayout->addWidget(close, 1, 0);
+	gridLayout->addWidget(open, 1, 1);
+	gridLayout->addWidget(retry, 1, 2);
  
     widget.show();
 
 	// uint64_t after = cpu_uJ();
 	// std::cout << (after-before)/time - baseline << "mW \n";
-	// return app.exec();
-	app.exec();
-	auto end = std::chrono::high_resolution_clock::now();   
-	auto curr_cpu = cpu_uJ();
-	auto curr_gpu = gpu_uJ();
-	double time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();   
-	std::cout << (curr_cpu - prev_cpu)/time - cpu_baseline << " " << (curr_gpu - prev_gpu)/time - gpu_baseline << "\n";	
+	return app.exec();
 }
