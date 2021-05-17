755f80e1197e377179e600dfde1990cd9bfa5795
David Freifeld <freifeld.david@gmail.com>
2021-05-17 00:49:18 -0700

    Separate QGroupBox population into individual funcs

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -18,81 +18,83 @@
 
 using json = nlohmann::json;
 
-void fill_env_box(QGroupBox* box, Func* main)
+void populate_env(QGroupBox* box, Func* main)
 {
 	curlpp::Cleanup clean;
 	const std::vector<std::string> api_codes {"CL", "PA", "WS", "WY", "SO", "WD", "NU", "OJ", "NG", "GE", "HV"};
 	const std::vector<std::string> api_names {"Coal", "Petroleum", "Waste", "Wind", "Solar", "Wood",
-		                                      "Nuclear", "Other Gases", "Natural Gas", "Geothermal", "Hydro"};
+											  "Nuclear", "Other Gases", "Natural Gas", "Geothermal", "Hydro"};
 	std::ifstream api_file("./api_file");
 	std::string api_key((std::istreambuf_iterator<char>(api_file)),  std::istreambuf_iterator<char>());
 	api_key.pop_back(); // HACK
 	for (int i = 0; i < api_codes.size(); i++) {
 		std::ostringstream os;
 		os << curlpp::options::Url("https://api.eia.gov/series/?api_key="+api_key+"&series_id=TOTAL."+api_codes[i]+"ETPUS.M"); // TODO Cache me.
-	    json raw_json = json::parse(os.str());
+		json raw_json = json::parse(os.str());
 		std::cout << api_names[i] << ": " << raw_json["series"][0]["data"][0][1] << "\n";
-	} 
+	}
 }
 
-int main(int argc, char *argv[]) 
+void populate_targets(QGroupBox* targets, std::vector<Func*> expensive, uint64_t total)
 {
-	setenv("XDG_RUNTIME_DIR", "/tmp/runtime-root", 0);
-	setenv("FONTCONFIG_PATH", "/etc/fonts", 0);	
-    Profiler prof(strtoll(argv[1], NULL, 10));
-    prof.start();   
-	// prof.dump(prof.funcs, 0);
-    auto expensive = prof.expensive_funcs({"Eigen", "std"});
-
-    QApplication app(argc, argv);   
-    QWidget widget;
-    widget.resize(640, 480);
-    widget.setWindowTitle("Hello, world!!!");
-
-    QGridLayout *gridLayout = new QGridLayout(&widget);
-
-    QPushButton* close = new QPushButton("&Close");
-	QPushButton* open = new QPushButton("Open Log");
-	QPushButton* retry = new QPushButton("Re-record");	
-
-	QObject::connect(close, &QPushButton::released, &QApplication::quit);
-    QObject::connect(open, &QPushButton::released, []{system("xdg-open ./latest.log");});
-	
-
-	QGroupBox* groupbox = new QGroupBox("envopt: indev 0.0");
-	groupbox->setFlat(true);
-	QVBoxLayout* vbox = new QVBoxLayout;
-	QGroupBox* targets = new QGroupBox("Primary Offenders");
-	QGroupBox* opt = new QGroupBox("Optimization Strategies");
-	QGroupBox* env = new QGroupBox("Environmental Impact");
-    QGridLayout* targets_layout = new QGridLayout;
+	QGridLayout* targets_layout = new QGridLayout;
 	targets->setLayout(targets_layout);
 	QVBoxLayout* names = new QVBoxLayout;
 	QVBoxLayout* percentages = new QVBoxLayout;
 	targets_layout->addLayout(names, 0, 0);
 	targets_layout->addLayout(percentages, 0, 1);
 	for (Func* f : expensive) {
-	    names->addWidget(new QLabel(QString::fromStdString(f->name)));
-		QString str;		
-		double percent = f->energy/static_cast<double>(prof.total);
+		names->addWidget(new QLabel(QString::fromStdString(f->name)));
+		QString str;
+		double percent = f->energy/static_cast<double>(total);
 		str.setNum(f->energy/1e6, 'g', 4);
 		QLabel* label = new QLabel(str+QString(" J"));
 		percentages->addWidget(label);
-	    label->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+		label->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
 		label->setMargin(3);
 		label->setStyleSheet("QLabel { background-color : rgba(255,0,0,"+QString::fromStdString(std::to_string(percent))+"); }");
 	}
-	// fill_env_box(env, expensive[0]);
+}
+
+int main(int argc, char *argv[])
+{
+	setenv("XDG_RUNTIME_DIR", "/tmp/runtime-root", 0);
+	setenv("FONTCONFIG_PATH", "/etc/fonts", 0);
+
+	Profiler prof(strtoll(argv[1], NULL, 10));
+	prof.start();
+	auto expensive = prof.expensive_funcs({"Eigen", "std"});
+
+	QApplication app(argc, argv);
+	QWidget widget;
+	widget.resize(640, 480);
+	widget.setWindowTitle("Hello, world!!!");
+
+	QGridLayout *gridLayout = new QGridLayout(&widget);
+  
+	QGroupBox* groupbox = new QGroupBox("envopt: indev 0.0");
+	QGroupBox* targets = new QGroupBox("Primary Offenders");
+	QGroupBox* opt = new QGroupBox("Optimization Strategies");
+	QGroupBox* env = new QGroupBox("Environmental Impact");	
+	QVBoxLayout* vbox = new QVBoxLayout;
 	vbox->addWidget(targets);
 	vbox->addWidget(opt);
 	vbox->addWidget(env);
 	groupbox->setLayout(vbox);
-    gridLayout->addWidget(groupbox, 0, 0, 1, 3);
-    gridLayout->addWidget(close, 1, 0);
-	gridLayout->addWidget(open, 1, 1);
-	gridLayout->addWidget(retry, 1, 2);
- 
-    widget.show();
+	gridLayout->addWidget(groupbox, 0, 0, 1, 3);
 
+	populate_targets(targets, expensive, prof.total);	
+	// populate_env(env, expensive[0]);
+	
+	QPushButton* close = new QPushButton("&Close");
+	QPushButton* open = new QPushButton("Open Log");
+	QPushButton* retry = new QPushButton("Re-record");
+	QObject::connect(close, &QPushButton::released, &QApplication::quit);
+	QObject::connect(open, &QPushButton::released, []{system("xdg-open ./latest.log");});
+	gridLayout->addWidget(close, 1, 0);
+	gridLayout->addWidget(open, 1, 1);
+	gridLayout->addWidget(retry, 1, 2);	
+
+	widget.show();
 	return app.exec();
 }
